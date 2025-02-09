961b02302c58324d4ea3652780c693328793a013
Linus Torvalds <136006237+worian4@users.noreply.github.com>
2025-02-09 18:28:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,26 +2,33 @@ from core.calc import Physics_Calculator
 
 
 
+def calculate(inp):
+    out = calc.calculate([i.split(' = ') for i in inp]) #to get calculated the input
+    for i in range(len(out[0])): print(out[0][i]+' = '+str(out[1][i]))
+def upgrade():
+    formulas = calc.generate_derivative_formulas()  #to upgrade derivative formulas
+
+    with open('res/derivative_formulas.txt', 'w') as file:
+        for i in formulas:
+            print(i)
+            file.write(' '.join(i)+'\n')
+
 der_formulas = open('res/derivative_formulas.txt', 'r').read().split('\n')
 formulas = open('res/base_formulas.txt', 'r').read().split('\n')
 consts = open('res/consts.txt', 'r').read().split('\n')
 sn_input = open('res/sn_input.txt', 'r').read().split('\n')
 sn_output = open('res/sn_output.txt', 'r').read().split('\n')
 
-inp = ['P = 400Wt', 'R = 10Om']  #examples of input
-inp1 = ['Q = 4200J', 'c = 4200J/kg', 't1 = 100C', 't2 = 102C']
-
 calc = Physics_Calculator(formulas, der_formulas, consts, sn_input, sn_output)
 
-'''
-formulas = calc.generate_derivative_formulas()  #to upgrade derivative formulas
+while 1:
+    command = input()
 
-with open('res/derivative_formulas.txt', 'w') as file:
-    for i in formulas:
-        print(i)
-        file.write(' '.join(i)+'\n')
-'''
-out = calc.calculate([i.split(' = ') for i in inp1])  #to get calculated the input
-
-for i in range(len(out[0])):
-    print(out[0][i]+' = '+str(out[1][i]))
+    if command[:9]=="calculate": #example: calculate(Q = 4200J,c = 4200J/kg*C,t1 = 100C,t2 = 102C)
+        try:
+            command = command.replace("calculate","").replace("(","").replace(")","").split(",")
+            calculate(command)
+        except Exception as e:
+            print(str(e))
+    elif command == "upgrade": upgrade()
+    else: print("command not found")
